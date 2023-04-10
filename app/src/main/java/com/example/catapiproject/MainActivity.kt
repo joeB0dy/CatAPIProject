51283db21b2f3952737f143850204210c86eac3e
joeB0dy <sjose@umich.edu>
2023-04-10 18:30:36 -0400

    Followed example off Guide.

--- app/src/main/java/com/example/catapiproject/MainActivity.kt
+++ app/src/main/java/com/example/catapiproject/MainActivity.kt
@@ -2,7 +2,14 @@ package com.example.catapiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.catapiproject.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -17,6 +24,32 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun printCatData(){
+        var catURL =    "https://api.thecatapi.com/v1/breeds" + "?api-key=live_jbbUIHNI836wIyACai0hKSLUeHcfqXv3DAV68q0c5xRGX7mvFJSP52DVwut4XgvY"
 
+        val queue = Volley.newRequestQueue(this)    //imported Volley.
+
+        //request a string response from provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, catURL,
+            { response ->
+                var catsArray : JSONArray = JSONArray(response)
+//indices from 0 through catsArray.length()-1
+                for(i in 0 until catsArray.length()) {
+//${} is to interpolate the string /
+                // uses a string template
+                    var theCat : JSONObject = catsArray.getJSONObject(i)
+    //now get the properties we want: name and description
+                    Log.i("MainActivity",
+                        "Cat name: ${theCat.getString("name")}")
+                    Log.i("MainActivity",
+                        "Cat description: ${theCat.getString("description")}")
+                }//end for
+            },
+           Response.ErrorListener {
+                Log.i("MainActivity", "THAT DIDN'T WORK... ")
+            })  //end of stringrequest.
+
+        //add to RequestQueue
+        queue.add(stringRequest)
     }//end printCatData
 }
